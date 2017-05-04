a9ccb51f434bee87e39c8e68145de63f57a950cf
Eugen Mayer <you@example.com>
2017-05-04 16:13:47 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style/base.less';
+
+ReactDOM.render(
+  <p>Hello World</p>,
+  document.getElementById('react-container')
+);
