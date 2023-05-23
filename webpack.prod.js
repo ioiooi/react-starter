a2c4de4082d53e8342aef13d96f77902e274564c
Eugen Mayer <eugen.mayer2@gmail.com>
2023-05-23 23:11:59 +0200

    kick define plugin from config and use --node-env environment option

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,5 @@
 const common = require("./webpack.common.js");
 const { merge } = require("webpack-merge");
-const webpack = require("webpack");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(common, {
@@ -31,9 +30,6 @@ module.exports = merge(common, {
   plugins: [
     new MiniCssExtractPlugin({
       filename: "[name].[chunkhash].css",
-    }),
-    new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify("production"),
     }),
   ],
 });
