e045fa92536708ff39d01ecd34823971e2c4799c
Eugen Mayer <eugen.mayer2@gmail.com>
2023-05-23 22:38:16 +0200

    update src/

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './components/App';
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+const el = document.getElementById("root");
+const root = ReactDOM.createRoot(el);
+
 root.render(<App />);
